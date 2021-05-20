92cd6f2ae6e284fa6f3eecb8a9af7b0e9e6ada03
eduard <diasfacundo@hotmail.com>
2021-05-20 07:53:55 -0500

    arreglado style moon

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,7 @@ let mountains_front = document.getElementById('mountains_front');
 window.addEventListener('scroll', function(){
       let value = window.scrollY;
       stars.style.left = value * 0.25 + 'px';
-      moon.style.top = value * 1.05 + 'px';
+       moon.style.left = value * 0.05 + 'px';
       mountains_behind.style.top = value * 0.5 + 'px';
       mountains_front.style.top = value * 0 + 'px';
       text.style.marginRight = value * 4 + 'px';
